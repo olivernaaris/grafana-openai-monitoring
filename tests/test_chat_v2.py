07bf6ee642e72e2d74611acb28a9fc54345b99a9
ishanjainn <ishan.jain0099@gmail.com>
2023-08-23 16:42:37 -0400

    adding tests

---
+++ tests/test_chat_v2.py
@@ -0,0 +1,22 @@
+import openai
+import os
+from grafana_openai_monitoring import monitor
+
+# Set your OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Apply the custom decorator to the OpenAI API function
+openai.ChatCompletion.create = monitor.chatV2(
+    openai.ChatCompletion.create,
+    metrics_url=os.getenv("PROMETHEUS_URL"),
+    logs_url=os.getenv("LOKI_URL"),
+    metrics_username=os.getenv("PROMETHEUS_USERNAME"),
+    logs_username=os.getenv("LOKI_USERNAME"),
+    access_token=os.getenv("GRAFANA_CLOUD_ACCESS_TOKEN")
+)
+
+# Now any call to openai.ChatCompletion.create will be automatically tracked
+response = openai.ChatCompletion.create(model="gpt-4", max_tokens=1, messages=[{"role": "user", "content": "What is Grafana?"}])
+
+# Assert on the response status code
+assert response['status'] == 'success'
