56bd0dc57840ac8d69c96d94a47c48f43c66ca4b
ishanjainn <ishan.jain0099@gmail.com>
2023-08-23 17:15:27 -0400

    updated test

--- tests/test_chat_v2.py
+++ tests/test_chat_v2.py
@@ -7,30 +7,30 @@ import os
 import openai
 from grafana_openai_monitoring import monitor
 
-# Set your OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+def test():
+    # Set your OpenAI API key
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Apply the custom decorator to the OpenAI API function
-openai.ChatCompletion.create = monitor.chat_v2(
-    openai.ChatCompletion.create,
-    metrics_url=os.getenv("PROMETHEUS_URL"),
-    logs_url=os.getenv("LOKI_URL"),
-    metrics_username=os.getenv("PROMETHEUS_USERNAME"),
-    logs_username=os.getenv("LOKI_USERNAME"),
-    access_token=os.getenv("GRAFANA_CLOUD_ACCESS_TOKEN")
-)
+    # Apply the custom decorator to the OpenAI API function
+    openai.ChatCompletion.create = monitor.chat_v2(
+        openai.ChatCompletion.create,
+        metrics_url=os.getenv("PROMETHEUS_URL"),
+        logs_url=os.getenv("LOKI_URL"),
+        metrics_username=os.getenv("PROMETHEUS_USERNAME"),
+        logs_username=os.getenv("LOKI_USERNAME"),
+        access_token=os.getenv("GRAFANA_CLOUD_ACCESS_TOKEN")
+    )
 
-# Now any call to openai.ChatCompletion.create will be automatically tracked
-response = openai.ChatCompletion.create(model="gpt-4",
-                                        max_tokens=1,
-                                        messages=
-                                            [
-                                                {
-                                                    "role": "user",
-                                                    "content": "What is Grafana?"
-                                                }
-                                            ]
-                                        )
-
-# Assert on the response status code
-assert response['object'] == 'chat.completion'
+    # Now any call to openai.ChatCompletion.create will be automatically tracked
+    response = openai.ChatCompletion.create(model="gpt-4",
+                                            max_tokens=1,
+                                            messages=
+                                                [
+                                                    {
+                                                        "role": "user",
+                                                        "content": "What is Grafana?"
+                                                    }
+                                                ]
+                                            )
+    
+    assert response['object'] == 'chat.completion'
